3f2c10d1cdf97dcaa436ab355aa7e568568dcb85
beasiteri <siteribea@gmail.com>
2026-02-28 17:23:59 +0100

    Refactor Chat API - Extracting chat service

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,16 +1,11 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import OpenAI from 'openai';
 import z from 'zod';
-import { conversationRepository } from './repositories/conversation.repository';
+import { chatService } from './services/chat.service';
 
 // Read all the variables
 dotenv.config();
-
-const client = new OpenAI({
-   apiKey: process.env.OPENAI_API_KEY,
-});
 
 const app = express();
 app.use(express.json());
@@ -44,19 +39,9 @@ app.post('/api/chat', async (req: Request, res: Response) => {
 
    try {
       const { prompt, conversationId } = req.body;
-      const response = await client.responses.create({
-         model: 'gpt-4o-mini!',
-         input: prompt,
-         temperature: 0.2,
-         max_output_tokens: 100,
-         previous_response_id:
-            conversationRepository.getLastResponseId(conversationId) ||
-            undefined,
-      });
+      const response = await chatService.sentdMessage(prompt, conversationId);
 
-      conversationRepository.setLastResponseId(conversationId, response.id);
-
-      res.json({ message: response.output_text });
+      res.json({ message: response.message });
    } catch (error) {
       console.error('Error communicating with OpenAI API:', error);
       res.status(500).json({ error: 'Failed to generate a response.' });
